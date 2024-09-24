155353ccd79a49d53ad625baf576bd0443dc51af
yitian_wang <2876768835@qq.com>
2024-09-24 20:50:05 +0800

    version1

--- app.py
+++ app.py
@@ -1,41 +1,273 @@
+import time
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 import inventory_manager as inv
+from datetime import datetime
 
-app = dash.Dash(__name__)
+today = datetime.now().date().isoformat()
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY, 'https://fonts.googleapis.com/css?family=Roboto&display=swap'], suppress_callback_exceptions=True)
 
-app.layout = html.Div([
-    html.H1('Mall Inventory Management System'),
-
-    # Section: View Products by Category
-    html.Div([
-        html.H2('Product Catalog'),
-        dcc.Dropdown(
-            id='category-dropdown',
-            options=[{'label': c, 'value': c} for c in inv.view_products_by_category().keys()],
-            placeholder="Select a category"
-        ),
-        html.Div(id='product-list')
-    ]),
-
-    # Section: Update Stock (Purchase/Sale)
-    html.Div([
-        html.H2('Update Stock'),
-        dcc.Input(id='product-id', type='text', placeholder='Product ID'),
-        dcc.Input(id='quantity', type='number', placeholder='Quantity'),
-        dcc.Dropdown(
-            id='operation-type',
-            options=[
-                {'label': 'Purchase', 'value': 'purchase'},
-                {'label': 'Sale', 'value': 'sale'}
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.DropdownMenu(
+            nav=True,
+            in_navbar=True,
+            label="Menu",
+            children=[
+                dbc.DropdownMenuItem("Product Catalog Management", id="page-1-link"),
+                dbc.DropdownMenuItem(divider=True),
+                dbc.DropdownMenuItem("Stock In/Out Management", id="page-2-link"),
+                dbc.DropdownMenuItem("Sale Analysis", id="page-3-link"),
+                dbc.DropdownMenuItem("Products List", id="page-4-link"),
+                dbc.DropdownMenuItem("Transactions List", id="page-5-link"),
             ]
         ),
-        dcc.Input(id='operator', type='text', placeholder='Operator'),
-        html.Button('Submit', id='submit-btn'),
-        html.Div(id='update-status')
-    ]),
+        dbc.NavItem(dbc.NavLink("New page", href="http://127.0.0.1:8050/", target="_blank")),
+        html.Div(style={'width': '30px'})  # 空位，宽度为40px（大约两个字符宽度）
+    ],
+    brand="Mall Inventory Management System"
+)
+
+html1 = dbc.Container([
+    dbc.Card([
+        dbc.CardHeader(html.H2('Product Catalog Management', className="display-5")),
+        dbc.CardBody([
+
+            # Section: Add or Delete a Product
+            html.H2('Update Product List'),
+            # Inputs in a row with product name field
+            html.Div([
+                dcc.Input(id='product-id-1', type='text', placeholder='Product ID (Add/Delete)',
+                          style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                dcc.Input(id='product-name-1', type='text', placeholder='Product Name (Add)',
+                          style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                dcc.Input(id='category-1', type='text', placeholder='Category (Add)',
+                          style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                dcc.Input(id='user-1', type='text', placeholder='User (Optional)',
+                          style={'width': '22%', 'height': '35px'})
+            ], style={'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center',
+                      'margin-bottom': '15px'}),
+
+            # Operation type dropdown
+            dcc.Dropdown(
+                id='operation-type-1',
+                options=[
+                    {'label': 'Add', 'value': 'add'},
+                    {'label': 'Delete', 'value': 'delete'}
+                ],
+                placeholder="Select operation type",
+                style={'width': '45%', 'margin-bottom': '15px'}
+            ),
+
+            # Submit button
+            html.Button('Submit', id='submit-btn-product', style={'margin-bottom': '15px'}),
+
+            # Update status
+            html.Div(id='update-product-status'),
+            html.Br(),
+
+            # Section: Product Catalog Lookup
+            html.Div([
+                html.H2('Product Catalog'),
+                dcc.Dropdown(
+                    id='category-dropdown',
+                    options=[{'label': c, 'value': c} for c in inv.view_products_by_category().keys()],
+                    placeholder="Select or input a category"
+                ),
+            ]),
+            html.Br(),
+            html.Div(id='product-list')
+        ])
+    ])
 ])
+
+html2 = dbc.Container([
+    dbc.Card([
+        dbc.CardHeader(html.H2('Product Catalog Management', className="display-5")),
+        dbc.CardBody([
+            # Section: Update Stock (Purchase/Sale)
+            html.Div([
+                html.H2('Update Stock'),
+
+                # Inputs in a row with product name field
+                html.Div([
+                    dcc.Input(id='product-id-2', type='text', placeholder='Product ID',
+                              style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='product-name-2', type='text', placeholder='Product Name',
+                              style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='quantity-2', type='number', placeholder='Quantity',
+                              style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='user-2', type='text', placeholder='User',
+                              style={'width': '22%', 'height': '35px'})
+                ], style={'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center',
+                          'margin-bottom': '15px'}),
+
+                # Operation type dropdown
+                dcc.Dropdown(
+                    id='operation-type-2',
+                    options=[
+                        {'label': 'Purchase', 'value': 'purchase'},
+                        {'label': 'Sale', 'value': 'sale'}
+                    ],
+                    placeholder="Select operation type",
+                    style={'width': '45%', 'margin-bottom': '15px'}
+                ),
+
+                # Submit button
+                html.Button('Submit', id='submit-btn-stock', style={'margin-bottom': '15px'}),
+                # Update status
+                html.Div(id='update-stock-status')
+            ]),
+            html.Br(),
+
+            # Section: Query Transaction
+            html.Div([
+                html.H2('Query Transaction'),
+
+                # Inputs in a row with product name field
+                html.Div([
+                    dcc.Input(id='product-id-3', type='text', placeholder='Product ID (Optional)',
+                              style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='user-3', type='text', placeholder='User (Optional)',
+                              style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='start-date-3', type='text', placeholder='YYYY-MM-DD', value=today[:10],
+                              style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='start-time-3', type='text', placeholder='HH:MM:SS (Start)',
+                              style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='end-date-3', type='text', placeholder='YYYY-MM-DD', value=today[:10],
+                              style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='end-time-3', type='text', placeholder='HH:MM:SS (End)',
+                              style={'width': '22%', 'height': '35px', 'margin-right': '2%'}),
+                ], style={'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center',
+                          'margin-bottom': '15px'}),
+
+                # Submit button
+                html.Button('Submit', id='submit-btn-transaction', style={'margin-bottom': '15px'}),
+                # Update status
+                html.Div(id='update-transaction-status')
+            ])
+        ])
+    ])
+])
+
+html3 = dbc.Container([
+    dbc.Card([
+        dbc.CardHeader(html.H2('Sale Analysis', className="display-5")),
+        dbc.CardBody([
+            # Section: Product Catalog Lookup
+            html.Div([
+                html.H2('Choose Filter'),
+                html.Div([
+                    dcc.Dropdown(id='category-4',
+                        options=[{'label': c, 'value': c} for c in inv.view_products_by_category().keys()],
+                        placeholder="Category (Optional)", style={'width': '90%', 'margin-right': '2%'}),
+                    dcc.Input(id='start-date-4', type='text', placeholder='YYYY-MM-DD', value=today[:10],
+                              style={'width': '50%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='start-time-4', type='text', placeholder='HH:MM:SS (Start)',
+                              style={'width': '50%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='end-date-4', type='text', placeholder='YYYY-MM-DD', value=today[:10],
+                              style={'width': '50%', 'height': '35px', 'margin-right': '2%'}),
+                    dcc.Input(id='end-time-4', type='text', placeholder='HH:MM:SS (End)',
+                              style={'width': '50%', 'height': '35px', 'margin-right': '2%'}),
+                ], style={'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center',
+                          'margin-bottom': '15px'}),
+
+                # Submit button
+                html.Button('Submit', id='submit-btn-sale', style={'margin-bottom': '15px'}),
+                # Update status
+                html.Div(id='sale-list')
+            ]),
+        ])
+    ])
+])
+
+html4 = dbc.Container([
+    dbc.Card([
+        dbc.CardHeader(html.H2('Products List', className="display-5")),
+        dbc.CardBody([
+            # Section: Product Catalog Lookup
+            html.Div([
+                html.Button('Refresh', id='refresh-1', style={'margin-bottom': '15px'}),
+                # Update status
+                html.Div(id='all-product-list')
+            ]),
+        ])
+    ])
+])
+
+html5 = dbc.Container([
+    dbc.Card([
+        dbc.CardHeader(html.H2('Transactions List', className="display-5")),
+        dbc.CardBody([
+            # Section: Product Catalog Lookup
+            html.Div([
+                html.Button('Refresh', id='refresh-2', style={'margin-bottom': '15px'}),
+                # Update status
+                html.Div(id='all-transaction-list')
+            ]),
+        ])
+    ])
+])
+
+app.layout = html.Div([
+    html.Link(
+        href='https://fonts.googleapis.com/css?family=Roboto&display=swap',  # Import Roboto font from Google Fonts
+        rel='stylesheet'
+    ),
+    navbar,
+    html.Div(id="page-content"),
+    html.Div(id="prediction-output", style={'display': 'none'})
+])
+
+
+def page_entry1():
+    return html1
+
+
+def page_entry2():
+    return html2
+
+
+def page_entry3():
+    return html3
+
+
+def page_entry4():
+    return html4
+
+
+def page_entry5():
+    return html5
+
+
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('page-1-link', 'n_clicks'),
+     Input('page-2-link', 'n_clicks'),
+     Input('page-3-link', 'n_clicks'),
+     Input('page-4-link', 'n_clicks'),
+     Input('page-5-link', 'n_clicks'),]
+)
+def display_page(page_1_clicks, page_2_clicks, page_3_clicks, page_4_clicks, page_5_clicks):
+    ctx = dash.callback_context
+
+    if not ctx.triggered:
+        return html1  # Default page1
+
+    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if button_id == "page-1-link":
+        return page_entry1()
+    elif button_id == "page-2-link":
+        return page_entry2()
+    elif button_id == "page-3-link":
+        return page_entry3()
+    elif button_id == "page-4-link":
+        return page_entry4()
+    elif button_id == "page-5-link":
+        return page_entry5()
 
 
 @app.callback(
@@ -45,19 +277,160 @@ app.layout = html.Div([
 def display_products(category):
     if category:
         products = inv.view_products_sorted_by_stock(category)
-        return html.Ul([html.Li(f"{p['name']}: {p['stock']} in stock") for p in products])
+        return "\n".join([f"{p['name']}: {p['stock']} in stock" for p in products])
     return "Select a category to view products."
 
 
 @app.callback(
-    Output('update-status', 'children'),
-    [Input('submit-btn', 'n_clicks')],
-    [Input('product-id', 'value'), Input('quantity', 'value'), Input('operation-type', 'value'),
-     Input('operator', 'value')]
+    [Output('product-id-2', 'value'),
+     Output('product-name-2', 'value')],
+    [Input('product-id-2', 'value'),
+     Input('product-name-2', 'value')],
 )
-def update_stock(n_clicks, product_id, quantity, operation_type, operator):
+def auto_fill_product_fields(product_id, product_name):
+    products = inv.read_json(inv.PRODUCTS_FILE)
+
+    # input product_id，automatically find product_name
+    if product_id:
+        if product_id in products:
+            return product_id, products[product_id]['name']
+        else:
+            return product_id, ''
+
+    # input product_name，automatically find product_id
+    elif product_name:
+        for p_id, details in products.items():
+            if details['name'].lower() == product_name.lower():
+                return p_id, product_name
+        return '', product_name
+
+    return '', ''
+
+
+@app.callback(
+    Output('update-product-status', 'children'),
+    [Input('submit-btn-product', 'n_clicks')],
+    [Input('product-id-1', 'value'),
+     Input('product-name-1', 'value'),
+     Input('category-1', 'value'),
+     Input('operation-type-1', 'value'),
+     Input('user-1', 'value')]
+)
+def update_product(n_clicks, product_id, product_name, category, operation_type, user):
     if n_clicks:
-        return inv.update_stock(product_id, quantity, operation_type, operator)
+        if operation_type == "add":
+            return inv.add_product(product_id, product_name, category, user)
+        elif operation_type == "delete":
+            return inv.delete_product(product_id, user)
+    return ""
+
+
+@app.callback(
+    Output('update-stock-status', 'children'),
+    [Input('submit-btn-stock', 'n_clicks')],
+    [Input('product-id-2', 'value'),
+     Input('product-name-2', 'value'),
+     Input('quantity-2', 'value'),
+     Input('operation-type-2', 'value'),
+     Input('user-2', 'value')]
+)
+def update_stock(n_clicks, product_id, product_name, quantity, operation_type, user):
+    if n_clicks:
+        if not operation_type:
+            return ""
+        return inv.update_stock(product_id, product_name, quantity, operation_type, user)
+    return ""
+
+
+def combine_date_and_time(start_date, start_time):
+    try:
+        date_obj = datetime.strptime(start_date, '%Y-%m-%d').date()
+    except ValueError:
+        return None
+
+    try:
+        time_obj = datetime.strptime(start_time, '%H:%M:%S').time()
+    except ValueError:
+        return None
+
+    combined_datetime = datetime.combine(date_obj, time_obj)
+    return combined_datetime.isoformat()
+
+
+@app.callback(
+    Output('update-transaction-status', 'children'),
+    [Input('submit-btn-transaction', 'n_clicks')],
+    [Input('product-id-3', 'value'),
+     Input('user-3', 'value'),
+     Input('start-date-3', 'value'),
+     Input('start-time-3', 'value'),
+     Input('end-date-3', 'value'),
+     Input('end-time-3', 'value'),]
+)
+def query_transaction(n_clicks, product_id, user, start_date, start_time, end_date, end_time):
+    if n_clicks:
+        if start_time:
+            start_time = combine_date_and_time(start_date, start_time)
+        else:
+            start_time = None
+        if end_time:
+            end_time = combine_date_and_time(end_date, end_time)
+        else:
+            end_time = None
+        if not (product_id or user or start_time or end_time):
+            return "Please select at least one filter."
+        if start_time and end_time:
+            if start_time > end_time:
+                return "Start time must be before end time."
+        transactions = inv.query_transactions(product_id, user, start_time, end_time)
+        text = "\n".join([f"product_id: {t['product_id']} product_name:{t['product_name']} operation_type: {t['operation_type']} operator: {t['operator']} timestamp: {t['timestamp']} quantity: {t['quantity']}" for t in transactions])
+        return text
+    return ""
+
+
+@app.callback(
+    Output('sale-list', 'children'),
+    [Input('submit-btn-sale', 'n_clicks')],
+    [Input('category-4', 'value'),
+     Input('start-date-4', 'value'),
+     Input('start-time-4', 'value'),
+     Input('end-date-4', 'value'),
+     Input('end-time-4', 'value'),]
+)
+def query_transaction(n_clicks, category, start_date, start_time, end_date, end_time):
+    if n_clicks:
+        if start_time:
+            start_time = combine_date_and_time(start_date, start_time)
+        else:
+            start_time = None
+        if end_time:
+            end_time = combine_date_and_time(end_date, end_time)
+        else:
+            end_time = None
+        if start_time and end_time:
+            if start_time > end_time:
+                return "Start time must be before end time."
+        return inv.sales_summary(category, start_time, end_time)
+    return ""
+
+
+@app.callback(
+    Output('all-product-list', 'children'),
+    [Input('refresh-1', 'n_clicks')]
+)
+def display_products(n_clicks):
+    if n_clicks:
+        return inv.display_all_products()
+    return ""
+
+
+@app.callback(
+    Output('all-transaction-list', 'children'),
+    [Input('refresh-2', 'n_clicks')]
+)
+def display_transactions(n_clicks):
+    if n_clicks:
+        return inv.display_all_transactions()
     return ""
 
 
--- inventory_manager.py
+++ inventory_manager.py
@@ -11,12 +11,12 @@ TRANSACTIONS_FILE = os.path.join(DATA_DIR, 'transactions.json')
 def read_json(file_path):
     if not os.path.exists(file_path):
         return {}
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
 def write_json(file_path, data):
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
 
 
@@ -33,12 +33,15 @@ def view_products_by_category():
 
 
 # 2. Stock Management (Purchase and Sale)
-def update_stock(product_id, quantity, operation_type, user):
+def update_stock(product_id, product_name, quantity, operation_type, user):
     products = read_json(PRODUCTS_FILE)
     transactions = read_json(TRANSACTIONS_FILE)
 
+    if not (product_id and product_name and quantity and operation_type and user):
+        return "Please input more information."
+
     if product_id not in products:
-        return "Product not found."
+        return "New product! Please operate product category first."
 
     if operation_type == "sale" and products[product_id]['stock'] < quantity:
         return "Insufficient stock."
@@ -52,21 +55,45 @@ def update_stock(product_id, quantity, operation_type, user):
     # Record transaction
     transaction = {
         "product_id": product_id,
-        "product_name": products[product_id]['name'],
+        "product_name": product_name,
         "operation_type": operation_type,
         "operator": user,
         "timestamp": datetime.now().isoformat(),
         "quantity": quantity
     }
-    transactions.append(transaction)
+    if not transactions:
+        transactions = [transaction]
+    else:
+        transactions.append(transaction)
 
     write_json(PRODUCTS_FILE, products)
     write_json(TRANSACTIONS_FILE, transactions)
     return "Stock updated successfully."
 
 
-# 3. Delete Product (Retain transaction history)
-def delete_product(product_id):
+# 3. Add or Delete Product (Retain transaction history)
+def add_product(product_id, product_name, category, user):
+    if not (product_id and product_name and category):
+        return "Please input more information."
+    products = read_json(PRODUCTS_FILE)
+    if product_id in products:
+        products[product_id]['name'] = product_name
+        write_json(PRODUCTS_FILE, products)
+        return "Product information changed successfully."
+    products[product_id] = {
+        'id': product_id,
+        'name': product_name,
+        'stock': 0,
+        'category': category,
+        'user': user
+    }
+    write_json(PRODUCTS_FILE, products)
+    return "Product added successfully."
+
+
+def delete_product(product_id, user):
+    if not product_id:
+        return "Please input more product ID."
     products = read_json(PRODUCTS_FILE)
     if product_id in products:
         del products[product_id]
@@ -84,23 +111,47 @@ def view_products_sorted_by_stock(category):
 
 
 # 5. Query Transaction Records
-def query_transactions(product_id=None, operator=None, start_time=None, end_time=None):
+def query_transactions(product_id=None, user=None, start_time=None, end_time=None):
     transactions = read_json(TRANSACTIONS_FILE)
     filtered_transactions = transactions
 
     if product_id:
         filtered_transactions = [t for t in filtered_transactions if t['product_id'] == product_id]
-    if operator:
-        filtered_transactions = [t for t in filtered_transactions if t['operator'] == operator]
-    if start_time and end_time:
-        filtered_transactions = [t for t in filtered_transactions if start_time <= t['timestamp'] <= end_time]
+    if user:
+        filtered_transactions = [t for t in filtered_transactions if t['operator'] == user]
+    if start_time:
+        filtered_transactions = [t for t in filtered_transactions if start_time <= t['timestamp']]
+    if end_time:
+        filtered_transactions = [t for t in filtered_transactions if t['timestamp'] <= end_time]
 
     return filtered_transactions
 
 
 # 6. Sales Summary
+def print_summary(summary):
+    output = []
+
+    if not summary:
+        output.append("No sales data available for the specified period.")
+        return "\n".join(output)
+
+    output.append("Sales Summary:")
+    output.append("-" * 30)
+    output.append(f"{'Product Name':<25} | {'Total Sold':<10}")
+    output.append("-" * 30)
+
+    for product_name, total_sold in summary.items():
+        output.append(f"{product_name:<25} | {total_sold:<10}")
+
+    output.append("-" * 30)
+    output.append("End of Summary.")
+
+    return "\n".join(output)
+
 def sales_summary(start_time, end_time, category=None):
     transactions = read_json(TRANSACTIONS_FILE)
+    if not transactions:
+        return "No transaction records available."
     sales = [t for t in transactions if t['operation_type'] == "sale" and start_time <= t['timestamp'] <= end_time]
 
     if category:
@@ -111,4 +162,60 @@ def sales_summary(start_time, end_time, category=None):
         product_name = sale['product_name']
         summary[product_name] = summary.get(product_name, 0) + sale['quantity']
 
-    return summary
+    return print_summary(summary)
+
+
+# 7. Display all products
+def display_all_products():
+    products = read_json(PRODUCTS_FILE)  # 假设读取的产品信息是一个字典
+    if not products:
+        return "No product records available."
+
+    sorted_products = {}
+
+    for product_id, product_info in products.items():
+        category = product_info['category']
+        if category not in sorted_products:
+            sorted_products[category] = []
+        sorted_products[category].append(product_info)
+
+    for category in sorted_products:
+        sorted_products[category].sort(key=lambda x: x['stock'], reverse=True)
+
+    output = []
+    output.append("All Products Information:")
+    output.append("-" * 40)
+
+    for category, items in sorted(sorted_products.items()):
+        output.append(f"Category: {category}")
+        output.append("-" * 40)
+        for item in items:
+            output.append(f"ID: {item['id']}, Name: {item['name']}, Stock: {item['stock']}, User: {item['user']}")
+
+        output.append("")
+
+    return "\n".join(output)
+
+
+# 8. Display all transactions
+def display_all_transactions():
+    transactions = read_json(TRANSACTIONS_FILE)
+
+    if not transactions:
+        return "No transaction records available."
+
+    sorted_transactions = sorted(transactions, key=lambda x: x['timestamp'], reverse=True)
+
+    output = []
+    output.append("All Transactions Information:")
+    output.append("-" * 40)
+
+    for transaction in sorted_transactions:
+        output.append(f"Product ID: {transaction['product_id']}, "
+                      f"Product Name: {transaction['product_name']}, "
+                      f"Operation Type: {transaction['operation_type']}, "
+                      f"Operator: {transaction['operator']}, "
+                      f"Timestamp: {transaction['timestamp']}, "
+                      f"Quantity: {transaction['quantity']}")
+
+    return "\n".join(output)
